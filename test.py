ab929ef885fa46a029d92c0bc5d1bf5c33a84337
Bhavith7 <bhavithp01@gmail.com>
2019-10-26 21:02:24 +0200

    Test Script for subtraction

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from fractions import Fraction
 n1 = [5,7.1,18/3,5/5,7.885]
 n2 = [7,8.67,8/23,6/6,7.511]
 
+add_from_script = []
+add_from_test = []
 sub_from_script = []
 sub_from_test = []
 
@@ -15,17 +17,30 @@ for i in range(len(n1)):
 
 	PARAM={"A":Fraction(n1[i]),"B":Fraction(n2[i])}
 	
+	test_add = n1[i] + n2[i]
+	add_from_test.append(test_add)
+
 	test_sub = n1[i] - n2[i]
 	sub_from_test.append(round(test_sub,3))
+
+	url_add = 'http://127.0.0.1:5000/add'
+	r = requests.get(url_add, params=PARAM)
+	data = r.json()
+	add_from_script.append(data)
 
 	url_sub = 'http://127.0.0.1:5000/sub'
 	r = requests.get(url_sub, params=PARAM)
 	data = r.json()
 	sub_from_script.append(round(data,3))
 
-	if sub_from_script[i] == sub_from_test[i]:
-		print "Tested successfully:OK"
+	if add_from_script[i] == add_from_test[i]:
+		print "Tested addition successfully:OK"
 	else:
-		print "Failed Test"
+		print "Failed addition Test"
+
+	if sub_from_script[i] == sub_from_test[i]:
+		print "Tested subtraction successfully:OK"
+	else:
+		print "Failed subtraction Test"
 
 
