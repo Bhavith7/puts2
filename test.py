6c8aafb13fd0b7d3937f9f30572b2bb92628cbd6
Bhavith7 <bhavithp01@gmail.com>
2019-10-26 23:46:45 +0200

    Updated test script for Subtraction operation

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ for i in range(len(n1)):
 	PARAM={"A":Fraction(n1[i]),"B":Fraction(n2[i])}
 	
 	test_add = n1[i] + n2[i]
-	add_from_test.append(test_add)
+	add_from_test.append(round(test_add,3))
 
 	test_sub = n1[i] - n2[i]
 	sub_from_test.append(round(test_sub,3))
@@ -26,7 +26,7 @@ for i in range(len(n1)):
 	url_add = 'http://127.0.0.1:5000/add'
 	r1 = requests.get(url_add, params=PARAM)
 	data1 = r1.json()
-	add_from_script.append(data1)
+	add_from_script.append(round(data1,3))
 
 	url_sub = 'http://127.0.0.1:5000/sub'
 	r2 = requests.get(url_sub, params=PARAM)
